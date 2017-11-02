229b60a2caec78c962923cc4e735168a684fe4fe
yddgit <642444906@qq.com>
2017-11-02 22:06:17 +0800

    Add a web API sample

--- www/urls.py
+++ www/urls.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from transwarp.web import get, view
+from apis import api, Page, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 from models import User, Blog, Comment
 
 @view('blogs.html')
@@ -13,4 +14,13 @@ def index():
     user = User.find_first('where email=?', 'tom@example.com')
     return dict(blogs=blogs, user=user)
 
+@api
+@get('/api/users')
+def api_get_users():
+    u'获取用户列表的API'
+    users = User.find_by('order by created_at desc')
+    # 把用户的口令隐藏掉
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
 
