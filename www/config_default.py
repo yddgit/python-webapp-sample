2669e493922eadf6e1ba7bda6b31a2d9ecb03b76
yddgit <642444906@qq.com>
2017-11-02 19:35:10 +0800

    Add config file

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+默认配置
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'database': 'test'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+覆盖配置
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+    }
+}
