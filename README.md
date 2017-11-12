4125d1df2deaededec39460124a47254845e05d8
yddgit <642444906@qq.com>
2017-11-12 21:23:25 +0800

    Add deployment script

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # python-webapp-sample
 A Python Webapp Sample
+
+## Dependencies
+* jinja2
+* mysql-connector-python
+* watchdog
+
+## Deployment
+
+1. 部署方式
+
+   WSGI服务器选择[gunicorn](http://gunicorn.org/)，它用类似Nginx的Master-Worker模式，同时可以提供gevent支持，不用修改代码即可获得极高的性能
+
+   Web服务器选择Nginx，可以处理静态资源，同时作为反向代理把动态请求交给gunicorn处理，gunicorn负责调用Python代码
+
+2. 服务端部署目录结构
+
+   ```
+   +--srv/
+      +--pws/          <--WebApp根目录
+         +--log/       <--存放log
+         +--www/       <--存放Python源码
+            +--static  <--存放静态资源文件
+   ```
+   考虑部署时版本更新与回退可能比较频繁，把www作为一个软链接，它指向哪个目录，哪个目录就是当前运行的版本，而Nginx和gunicorn的配置文件只需要指向www目录即可
+
+3. Nginx可以作为服务进程直接启动，但gunicorn还不行，可以使用[Supervisor](http://supervisord.org/)。Supervisor是一个管理进程的工具，可以随系统启动而启动服务，还可以时刻监控服务进程，如果服务进程意外退出，Supervisor可以自动重启服务
+
+4. 安装相关服务
+
+   * Nginx：高性能Web服务器+负责反向代理
+   * gunicorn：高性能WSGI服务器
+   * gevent：把Python同步代码变成异步协程的库
+   * Supervisor：监控服务进程的工具
+   * MySQL：数据库服务
+
+   以下安装命令均在CentOS 7.3 x64上执行
+
+   ```bash
+   # 安装相关服务和依赖
+   yum install nginx
+   yum install python-gevent
+   yum install supervisor
+   yum install python-gunicorn
+   # 如果当前系统的yum源中没有MySQL，则需要添加repo源
+   #wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
+   #rpm -ivh mysql-community-release-el7-5.noarch.rpm
+   yum install mysql-server
+   yum install python-jinja2
+   yum install mysql-connector-python
+
+   # 创建服务端部署目录
+   mkdir /srv/pws
+   useradd www-data
+   passwd www-data
+   chown www-data:www-data pws
+
+   # 初始化数据库，如果root用户没有密码可以不用加-p参数
+   systemctl start mysql
+   mysql -u root -p < schema.sql
+
+   # 启动supervisor
+   systemctl start supervisord
+   # 启动nginx
+   systemctl start nginx
+   ```
+
+5. 配置部署
+
+   使用工具配合脚本完成自动化部署，[Fabric](http://www.fabfile.org/)是一个自动化部署工具，是用Python开发的，所以部署脚本也是用Python来编写
+
+   要在开发机（不是服务器）上安装Fabric
+   ```
+   pip install fabric
+   ```
+
+6. 使用supervisor启停服务
+
+   ```bash
+   supervisorctl stop pws
+   supervisorctl start pws
+   supervisorctl status
+   systemctl restart nginx
+   ```
+
+7. 配置nginx支持https访问
+
+   这里使用[Let’s Encrypt](https://letsencrypt.org/)配置https
+
+   * 访问[Certbot](https://certbot.eff.org/)，按照首页提示，选择Server和操作系统版本，会显示相应的操作步骤
+   * 例如Nginx+CentOS7：https://certbot.eff.org/#centosrhel7-nginx
+
---
+++ fabfile.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+u'部署脚本'
+
+import os, re
+from datetime import datetime
+
+from fabric.api import *
+
+# 服务器登录用户名
+env.user = 'www-data'
+# sudo用户为root
+env.sudo_user = 'root'
+# 服务器地址，可以有多个，依次部署
+env.hosts = ['192.168.1.100']
+
+# 服务器MySQL用户名和口令
+db_user = 'www-data'
+db_password = '******'
+
+_TAR_FILE = 'dist-pws.tar.gz'
+_REMOTE_TMP_TAR = '/tmp/%s' % _TAR_FILE
+_REMOTE_BASE_DIR = '/srv/pws'
+
+def _current_path():
+    u'本地当前路径'
+    return os.path.abspath('.')
+
+def _now():
+    u'当前时间'
+    return datetime.now().strftime('%y-%m-%d_%H.%M.%S')
+
+def backup():
+    '''
+    备份服务端数据库到本地
+    '''
+    dt = _now()
+    f = 'backup-pws-%s.sql' % dt
+    with cd('/tmp'):
+        run('mysqldump --user=%s --password=%s --skip-opt --add-drop-table --default-character-set=utf8 --quick pws > %s' % (db_user, db_password, f))
+        run('tar -czvf %s.tar.gz %s' % (f, f))
+        get('%s.tar.gz' % f, '%s/backup/' % _current_path())
+        run('rm -f %s' % f)
+        run('rm -f %s.tar.gz' % f)
+
+# 每个Python函数都是一个任务，先编写一个打包任务
+def build():
+    u'打包任务'
+    includes = ['static', 'templates', 'transwarp', 'favicon.ico', '*.py']
+    excludes = ['test', '.*', '*.pyc', '*.pyo']
+    # local('...')运行本地命令
+    local('rm -f dist/%s' % _TAR_FILE)
+    # 将当前命令的目录设定为lcd(path)指定的目录
+    with lcd(os.path.join(_current_path(), 'www')):
+        cmd = ['tar', '--dereference', '-czvf', '../dist/%s' % _TAR_FILE]
+        cmd.extend(['--exclude=\'%s\'' % ex for ex in excludes])
+        cmd.extend(includes)
+        local(' '.join(cmd))
+
+# 编写deploy任务，把打包文件上传至服务器，解压，重置软链接，重启相关服务
+def deploy():
+    u'部署任务'
+    newdir = 'www-%s' % _now()
+    # 删除已有的tar文件
+    run('rm -f %s' % _REMOTE_TMP_TAR)
+    # 上传新的tar文件
+    put('dist/%s' % _TAR_FILE, _REMOTE_TMP_TAR)
+    # 创建新目录
+    with cd(_REMOTE_BASE_DIR):
+        sudo('mkdir %s' % newdir)
+    # 解压到新目录
+    with cd('%s/%s' % (_REMOTE_BASE_DIR, newdir)):
+        sudo('tar -xzvf %s' % _REMOTE_TMP_TAR)
+    # 重置软链接
+    with cd(_REMOTE_BASE_DIR):
+        sudo('rm -f www')
+        sudo('ln -s %s www' % newdir)
+        sudo('chown www-data:www-data www')
+        sudo('chown -R www-data:www-data %s' % newdir)
+    # 重启Python服务和nginx服务器
+    with settings(warn_only=True):
+        sudo('supervisorctl stop pws')
+        sudo('supervisorctl start pws')
+        sudo('systemctl restart nginx')
+
+# run()函数执行的命令是在服务器上运行，with cd(path)和with lcd(path)类似，把当前目录在服务器端设置为cd()指定的目录
+# 如果一个命令需要sudo权限，就不能用run()，而是用sudo()来执行
+
+RE_FILES = re.compile('\r?\n')
+
+def rollback():
+    u'''
+    回滚到上一个版本
+    '''
+    with cd(_REMOTE_BASE_DIR):
+        r = run('ls -p -1')
+        files = [s[:-1] for s in RE_FILES.split(r) if s.startswith('www-') and s.endswith('/')]
+        files.sort(cmp=lambda s1, s2: 1 if s1 < s2 else -1)
+        r = run('ls -l www')
+        ss = r.split(' -> ')
+        if len(ss) != 2:
+            print ('ERROR: \'www\' is not a symbol link.')
+            return
+        current = ss[1]
+        print ('Found current symbol link points to: %s\n' % current)
+        try:
+            index = files.index(current)
+        except ValueError, e:
+            print ('ERROR: symbol link is invalid.')
+            return
+        if len(files) == index + 1:
+            print ('ERROR: already the oldest version.')
+        old = files[index + 1]
+        print ('==================================================')
+        for f in files:
+            if f == current:
+                print ('      Current ---> %s' % current)
+            elif f == old:
+                print ('  Rollback to ---> %s' % old)
+            else:
+                print ('                   %s' % f)
+        print ('==================================================')
+        print ('')
+        yn = raw_input ('continue? y/N ')
+        if yn != 'y' and yn != 'Y':
+            print ('Rollback cancelled.')
+            return
+        print ('Start rollback...')
+        sudo('rm -f www')
+        sudo('ln -s %s www' % old)
+        sudo('chown www-data:www-data www')
+        with settings(warn_only=True):
+            sudo('supervisorctl stop pws')
+            sudo('supervisorctl start pws')
+            sudo('systemctl restart nginx')
+        print ('ROLLBACKED OK.')
+
+def restore2local():
+    u'''
+    将服务端数据库恢复到本地
+    '''
+    backup_dir = os.path.join(_current_path(), 'backup')
+    fs = os.listdir(backup_dir)
+    files = [f for f in fs if f.startswith('backup-') and f.endswith('.sql.tar.gz')]
+    files.sort(cmp=lambda s1, s2: 1 if s1 < s2 else -1)
+    if len(files)==0:
+        print 'No backup files found.'
+        return
+    print ('Found %s backup files:' % len(files))
+    print ('==================================================')
+    n = 0
+    for f in files:
+        print ('%s: %s' % (n, f))
+        n = n + 1
+    print ('==================================================')
+    print ('')
+    try:
+        num = int(raw_input ('Restore file: '))
+    except ValueError:
+        print ('Invalid file number.')
+        return
+    restore_file = files[num]
+    yn = raw_input('Restore file %s: %s? y/N ' % (num, restore_file))
+    if yn != 'y' and yn != 'Y':
+        print ('Restore cancelled.')
+        return
+    print ('Start restore to local database...')
+    p = raw_input('Input mysql root password: ')
+    sqls = [
+        'drop database if exists test;',
+        'create database test;',
+        'grant all privileges on test.* to \'%s\'@\'localhost\' identified by \'%s\';' % (db_user, db_password)
+    ]
+    for sql in sqls:
+        local(r'mysql -uroot -p%s -e "%s"' % (p, sql))
+    with lcd(backup_dir):
+        local('tar zxvf %s' % restore_file)
+    local(r'mysql -uroot -p%s test < backup/%s' % (p, restore_file[:-7]))
+    with lcd(backup_dir):
+        local('rm -f %s' % restore_file[:-7])
+
